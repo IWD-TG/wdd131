7751f59d97fba67f2c8cabe265bd326984150535
IWD-TG <gggrossfruitland@gmail.com>
2025-05-27 14:21:50 -0600

    made html adapt with js

--- projects/book-blog/js/blog.js
+++ projects/book-blog/js/blog.js
@@ -9,7 +9,7 @@ const articles = [
 		imgAlt: 'Book cover for Septimus Heap 1',
 		ages: '10-14',
 		genre: 'Fantasy',
-		stars: '****'
+		stars: '⭐⭐⭐⭐'
 	},
 	{
 		id: 2,
@@ -23,21 +23,65 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
 ]
 
-console.log(articles[0].first);
+/*console.log(articles[0].first);*/
 
-let container = document.querySelector()
+let container = document.querySelector("#bookList")
 
 articles.forEach(function(item) {
 
     let book = document.createElement("section")
-    let html= `
-    <p>${item.first}</p>
-    <p>${item.last}<p>
-
-    `
+    let html= 
+		`<div class="secondContainer">
+                <div class="aboutContainer">
+                    <h4>${item.date}</h4>
+                    <p>
+                    ${item.ages}<br>
+                    ${item.genre}<br>
+                    ${item.stars}
+                    </p>
+                </div>
+                <div class="bookContainer">
+                    <h2>${item.title}</h2>
+                    <img src="${item.imgSrc}" alt="${item.imgAlt}">
+                    <p>${item.description}</p>
+                </div>
+            </div>`;
+			
     book.innerHTML = html
     container.appendChild(book)
+	/*<p>${item.first}</p>
+    <p>${item.last}<p>*/
 })
+/*<div id="bookList">
+        
+            <div class="secondContainer">
+                <div class="aboutContainer">
+                    <h4>July 5, 2022</h4>
+                    <p>
+                    10–14<br>
+                    Fantasy<br>
+                    ★★★★
+                    </p>
+                </div>
+                <div class="bookContainer">
+                    <h2>Septimus Heap Book One: Magyk</h2>
+                    <img src="https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg" alt="Book cover for Septimus Heap 1">
+                    <p>If you enjoy stories about seventh sons of seventh sons and magyk, this is the book for you.</p>
+                </div>
+            </div>*/
